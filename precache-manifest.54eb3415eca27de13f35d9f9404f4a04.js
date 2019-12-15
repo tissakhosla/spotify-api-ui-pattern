4a5a7284791adb177cae963e75755fb63584847c
Tissa Khosla <tissa.music@gmail.com>
2019-12-15 18:55:33 -0500

    Updates

---
+++ precache-manifest.54eb3415eca27de13f35d9f9404f4a04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c07d83b657c48c6620b88f0d9fd317d",
+    "url": "/spotify-api-ui-pattern/index.html"
+  },
+  {
+    "revision": "9c992e4e9c66c635eaa0",
+    "url": "/spotify-api-ui-pattern/static/css/main.235e0ea8.chunk.css"
+  },
+  {
+    "revision": "8b8d15d7d5dc6686bf2a",
+    "url": "/spotify-api-ui-pattern/static/js/2.befd4099.chunk.js"
+  },
+  {
+    "revision": "9c992e4e9c66c635eaa0",
+    "url": "/spotify-api-ui-pattern/static/js/main.397db80e.chunk.js"
+  },
+  {
+    "revision": "cc0b5bf2f39e6643f113",
+    "url": "/spotify-api-ui-pattern/static/js/runtime-main.7f62d3a5.js"
+  }
+]);
